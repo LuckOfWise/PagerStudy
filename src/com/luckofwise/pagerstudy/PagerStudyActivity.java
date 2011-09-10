9b0e67d25bc01aaf629ac1efb7e264cf887d618a
野上 誠司 <wise@luck.local>
2011-09-10 18:41:12 +0900

    Change button to radio button.

--- src/com/luckofwise/pagerstudy/PagerStudyActivity.java
+++ src/com/luckofwise/pagerstudy/PagerStudyActivity.java
@@ -3,11 +3,14 @@ package com.luckofwise.pagerstudy;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.View;
+import android.widget.RadioGroup;
 
-public class PagerStudyActivity extends FragmentActivity {
+public class PagerStudyActivity extends FragmentActivity implements OnPageChangeListener {
 
     ViewPager mViewPagerMain;
+    RadioGroup mRadioGroupTabs;
 
     /** Called when the activity is first created. */
     @Override
@@ -16,6 +19,9 @@ public class PagerStudyActivity extends FragmentActivity {
         setContentView(R.layout.main);
         mViewPagerMain = (ViewPager) findViewById(R.id.viewPagerMain);
         mViewPagerMain.setAdapter(new MyAdapter(this.getSupportFragmentManager()));
+        mViewPagerMain.setOnPageChangeListener(this);
+        mRadioGroupTabs = (RadioGroup) findViewById(R.id.radioGroupTabs);
+        mRadioGroupTabs.check(R.id.buttonFirst);
     }
 
     public void onClick(View v) {
@@ -30,5 +36,30 @@ public class PagerStudyActivity extends FragmentActivity {
             mViewPagerMain.setCurrentItem(MyAdapter.ITEM_THIRD);
             break;
         }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        switch (position) {
+        case MyAdapter.ITEM_FIRST:
+            mRadioGroupTabs.check(R.id.buttonFirst);
+            break;
+        case MyAdapter.ITEM_SCOND:
+            mRadioGroupTabs.check(R.id.buttonSecond);
+            break;
+        case MyAdapter.ITEM_THIRD:
+            mRadioGroupTabs.check(R.id.buttonThird);
+            break;
+            
+        }
+        
     }
 }
